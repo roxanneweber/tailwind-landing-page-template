ee41a5bd4b6bc01eb6d1a217da41e4756afca2fb
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-05 02:11:32 -0500

    completed a one-page template for the daily image with tailwind

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function HeroHome() {
+	const [daily, setDaily] = useState([]);
+
 	return (
 		<section className='relative'>
 			{/* Illustration behind hero content */}
@@ -71,92 +74,26 @@ function HeroHome() {
 							data-aos='zoom-y-out'
 							data-aos-delay='450'
 						>
-							<div className='flex flex-col justify-center'>
-								<img
-									className='mx-auto'
-									src={require('../images/hero-image.png').default}
-									width='768'
-									height='432'
-									alt='Hero'
-								/>
-								<svg
-									className='absolute inset-0 max-w-full mx-auto md:max-w-none h-auto'
-									width='768'
-									height='432'
-									viewBox='0 0 768 432'
-									xmlns='http://www.w3.org/2000/svg'
-									xmlnsXlink='http://www.w3.org/1999/xlink'
-								>
-									<defs>
-										<linearGradient
-											x1='50%'
-											y1='0%'
-											x2='50%'
-											y2='100%'
-											id='hero-ill-a'
-										>
-											<stop stopColor='#FFF' offset='0%' />
-											<stop stopColor='#EAEAEA' offset='77.402%' />
-											<stop stopColor='#DFDFDF' offset='100%' />
-										</linearGradient>
-										<linearGradient
-											x1='50%'
-											y1='0%'
-											x2='50%'
-											y2='99.24%'
-											id='hero-ill-b'
-										>
-											<stop stopColor='#FFF' offset='0%' />
-											<stop stopColor='#EAEAEA' offset='48.57%' />
-											<stop
-												stopColor='#DFDFDF'
-												stopOpacity='0'
-												offset='100%'
-											/>
-										</linearGradient>
-										<radialGradient
-											cx='21.152%'
-											cy='86.063%'
-											fx='21.152%'
-											fy='86.063%'
-											r='79.941%'
-											id='hero-ill-e'
-										>
-											<stop stopColor='#4FD1C5' offset='0%' />
-											<stop stopColor='#81E6D9' offset='25.871%' />
-											<stop stopColor='#338CF5' offset='100%' />
-										</radialGradient>
-										<circle
-											id='hero-ill-d'
-											cx='384'
-											cy='216'
-											r='64'
-										/>
-									</defs>
-									<g fill='none' fillRule='evenodd'>
-										<circle
-											fillOpacity='.04'
-											fill='url(#hero-ill-a)'
-											cx='384'
-											cy='216'
-											r='128'
-										/>
-										<circle
-											fillOpacity='.16'
-											fill='url(#hero-ill-b)'
-											cx='384'
-											cy='216'
-											r='96'
-										/>
-										<g fillRule='nonzero'>
-											<use fill='#000' xlinkHref='#hero-ill-d' />
-											<use
-												fill='url(#hero-ill-e)'
-												xlinkHref='#hero-ill-d'
-											/>
-										</g>
-									</g>
-								</svg>
+							{useEffect(() => {
+								axios
+									.get(
+										'https://api.nasa.gov/planetary/apod?api_key=Rz30JyD2a2ysf2lhBMAwC9RgnPpsq0Jliv0jScXG'
+									)
+									.then((res) => {
+										console.log(res.data);
+										setDaily(res.data);
+									})
+									.catch((err) => console.error(err));
+							}, [])}
+
+							<div>
+								<h1>{daily.title}</h1>
+								<h3>
+									{daily.copyright} - {daily.date}
+								</h3>
+								<div>
+									<img src={daily.url} alt='a daily NASA photograph' />
+								</div>
 							</div>
 						</div>
 					</div>
